e8ffc3db828b6080ae6bccee40b451dd630bde6f
Petr Ochvat <petrochvat@gmail.com>
2020-05-31 20:16:08 +0000

    add new changes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,38 @@
 import React from 'react'
+import { render } from '@testing-library/react';
 
-function Movie(props) {
-    console.log(props.result.original_name)
+function Movie({result, openPopup}) {
     const img_url = "http://image.tmdb.org/t/p/w500/";
     let moviename = ''
-    if (props.result.title) {
-        moviename = props.result.title
+    let gender = ''
+    if (result.title) {
+        moviename = result.title
     }
     else {
-        moviename = props.result.name
+        moviename = result.name
     }
+
+    if (result.gender) {
+        gender = result.gender
+    }
+    
+    // else if (99 in result.genres) {
+    //     gender = 'documentary'
+    // }
+    else {
+        gender = result.media_type
+    }
+    console.log(result.genre_ids)
+    if (result.genre_ids.find(99)) {
+        gender = 'documentary'
+    }
+    // if (result.last_episode_to_air)
     return (
-        <div className="result">
-            <img src={img_url+props.result.poster_path}/>
+        <div className="result" onClick={() => openPopup(result.id, gender)} >
+            <img src={img_url+result.poster_path}/>
             <h3>{moviename}</h3>
-        
+            <h3> Ranking: {result.vote_average}</h3>
+            <h3>Gender: {gender}</h3>
         </div>
     )
     
